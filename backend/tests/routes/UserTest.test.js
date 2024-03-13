f332a3ce283ea3643ae3f0b40770769bbaba131c
ismael200200 <826134@unizar.es>
2024-03-13 15:45:48 +0100

    continuacion pruebas

--- backend/tests/routes/UserTest.test.js
+++ backend/tests/routes/UserTest.test.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 // Importa el router que deseas probar
-const userRoutes = require('../routes/userRoutes');
+const userRoutes = require('../../routes/userRoutes');
 const request = require('supertest');
 // Configura una ruta de prueba para verificar la autenticación
 app.use('/test', userRoutes);
@@ -21,7 +21,7 @@ console.log('Token de prueba:', t);
 describe('Pruebas de la API de autenticación de Google', () => {
   it('debería autenticar correctamente un token válido', async () => {
     const response = await request(app)
-      .post('/test/auth')
+      .post('/backend/app.js')
       .send({ token: t });
 
     expect(response.status).toBe(200);
@@ -31,11 +31,11 @@ describe('Pruebas de la API de autenticación de Google', () => {
   it('debería devolver un error para un token inválido', async () => {
     const invalidToken = 'token_invalido'; // Reemplaza con un token inválido
     const response = await request(app)
-      .post('/test/auth')
+      .post('/backend/app.js')
       .send({ token: invalidToken });
 
     expect(response.status).toBe(401);
-  });
+  }, 10000);
 
   it('debería obtener la información del usuario autenticado', async () => {
     const userId = uId; // Reemplaza con el ID correcto
@@ -46,6 +46,6 @@ describe('Pruebas de la API de autenticación de Google', () => {
     expect(response.status).toBe(200);
     expect(response.body.email).toBe(uEmail); // Reemplaza con el correo correcto
     // Agrega más expectativas según tus necesidades
-  });
+  }, 10000);
 });
 
