d30a3cb771e8afdc22583754915885ddc1664941
akarafy <850111@unizar.es>
2024-03-29 16:30:38 +0100

    Habilitad cors con localhost

--- backend/app.js
+++ backend/app.js
@@ -5,18 +5,28 @@ const cors = require('cors');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-
 const https = require('https');
 const fs = require('fs');
 
 require('./middleware/authGoogle');
-const jwt = require('jsonwebtoken');
 
+
+//For security reasons we only allow origins from our client url and localhost port 3000
+const allowedOrigins = [process.env.CLIENT_URL, 'http://localhost:3000'];
 //Enable comuniccation with our fronted
 app.use(
   cors({
+    origin: function(origin, callback){
+      // allow requests with no origin (like mobile apps or curl requests)
+      if(!origin) return callback(null, true);
+      if(allowedOrigins.indexOf(origin) === -1){
+        var msg = 'The CORS policy for this site does not ' +
+                  'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     credentials: true,
-    origin: process.env.CLIENT_URL,
   })
 );
 
