ebe5b5a39fe788d5c096b04480bb30b8bc5a6f1e
ismael200200 <95617676+ismael200200@users.noreply.github.com>
2024-04-10 19:47:45 +0200

    arreglado broadcast

--- backend/middleware/game.js
+++ backend/middleware/game.js
@@ -101,7 +101,11 @@ function leaveRoom(socket, user) {
 
 // Send a message to all users in a room, note: doesnt send to itself
 function socketBroadcastToRoom(socket, event, room, data) {
-  socket.to(room).emit(event,data)
+  rooms.get(room).forEach((sid) => {
+    if (sid !== socket.id) {
+      socketEmit(socket, event, room);
+    }
+  });
 }
 
 // Send a message to a specific user
