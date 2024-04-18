ffd98937fe3a72195555fbc31ae4e31b98f2afd0
ismael200200 <826134@unizar.es>
2024-04-18 22:10:50 +0200

    enviar correo tambien

--- backend/middleware/game.js
+++ backend/middleware/game.js
@@ -235,9 +235,8 @@ function chat(socket, emailToSocket, user, message) {
   if (sids.has(user.email)) {
     let userCode = sids.get(user.email).code;
     console.log("message to be sent");
-    console.log(message)
-
-    sendToAllWithCode(emailToSocket, userCode, 'messageReceived', message);
+    console.log(message);
+    sendToAllWithCode(emailToSocket, userCode, 'messageReceived', { message, user: user.email });
   } else {
     console.log(`You are not in a Room  ` + user.email);
     socketEmit(socket, 'notInARoom', user.email);
