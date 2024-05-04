d2a5aff8728ec442102256b66a816e975af0040e
akarafy <850111@unizar.es>
2024-05-04 14:31:49 +0200

    Base de datos mejorada

---
+++ BBDD/WealthWar_insert_achievement.sql
@@ -0,0 +1,10 @@
+INSERT INTO Achievement (title, description, image_url) VALUES 
+('Bienvenido a WealthWars', 'Juega tu primera partida', 'https://i.ibb.co/J23YLFk/jugar-1-partida.jpg'),
+('Comandante principiante', 'Gana tu primera partida', 'https://i.ibb.co/dPRg2b5/ganar-1-partida.jpg'),
+('Comandante experimentado', 'Gana 10 partidas', 'https://i.ibb.co/tC45TcR/ganar-10-partidas.jpg'),
+('Comandante veterano', 'Gana 100 partidas', 'https://i.ibb.co/YR21t9j/ganar-100-partidas.jpg'),
+('Tu primer compañero', 'Haz tu primer amigo', 'https://i.ibb.co/c2pgkLy/a-adir-1-amigo.jpg'),
+('Conquistador', 'Conquista un territorio en una partida', 'https://i.ibb.co/qCzKMfX/conquista-1-territorio.jpg'),
+('Industrializador', 'Compra una fabrica en una partida', 'https://i.ibb.co/3rK0JzM/comprar-1-fabrica.jpg'),
+('Revolucion industrial', 'Obten 15 partidas en una sola partida', 'https://i.ibb.co/2v7Zsbp/comprar-15-fabricas.jpg'),
+('La Armada Invencible', 'Alcanza a tener 99 tropas en un solo territorio', 'https://i.ibb.co/N7CtJPC/conseguir-99-tropas.jpg');
