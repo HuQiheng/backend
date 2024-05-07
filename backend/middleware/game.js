59cec6a8d07b280e3b1a0e558e946a6683d777b5
ismael200200 <826134@unizar.es>
2024-05-07 18:07:06 +0200

    arreglo logro mileurista

--- backend/middleware/game.js
+++ backend/middleware/game.js
@@ -196,7 +196,7 @@ async function nextTurnHandler(socket, emailToSocket, user) {
     const playerIndex = state.players.findIndex((p) => p.email.trim() === user.email.trim());
     // If reach 1000 coins, unlock an achievement
     if(state.players[playerIndex].coins >= 1000) {
-      await giveAchievement(emailToSocket,'Mileurista', user.email)
+      await giveAchievement(emailToSocket,'Mileurista', user.email);
     }
   } else {
     console.log(`You are not in a Room  ` + user.email);
